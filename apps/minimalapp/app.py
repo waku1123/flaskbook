bdf0b14bfe2af7d1895f23e34be7721cf22f3889
yss <waku.pdxppxzy@gmail.com>
2022-08-23 01:06:07 +0900

    feat: template and static files

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,3 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello/<string:name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello World! {name}"
+
+
+@app.route("/name/<string:name>")
+def show_name(name: str):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # to /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
